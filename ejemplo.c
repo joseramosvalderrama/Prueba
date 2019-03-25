5023268079e5c70633ed72cb7112ba29bd8b5d2a
joseramosvalderrama <jose.ramos.valderrama@alumnos.upm.es>
2019-03-25 08:47:07 +0100

    Cuanto toy

--- ejemplo.c
+++ ejemplo.c
@@ -2,14 +2,6 @@
 
 void main(){
 
-<<<<<<< HEAD
-	printf("Diego vamos instala git, vaya cafre. EM Vamos Alex");
-=======
-	printf("Diego vamos instala git, vaya cafre.");
-	printf("prueba alex");
-<<<<<<< HEAD
-	printf("prueba diego");
-=======
->>>>>>> origin/master
->>>>>>> origin/master
+	printf("Sois muy toys");
+	
 }
